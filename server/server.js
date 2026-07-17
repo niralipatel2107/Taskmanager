43f4a942ef2013a4acf5ff3e32f3d3a338a5ef8e
niralipatel2107 <niralipatel2801@gmail.com>
2026-07-16 21:07:14 -0400

    Connect MongoDB and configure Express server

--- server/server.js
+++ server/server.js
@@ -1,25 +1,28 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 dotenv.config();
+connectDB();
 
 const app = express();
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req,res)=>{
-    res.send("Team Issue tracker backend is running")
+app.get("/", (req, res) => {
+  res.send("Team Issue Tracker backend is running");
 });
 
-app.get("api/health",(req,res)=>{
-    res.json({
-        message: "api is healthy",
-        status:"OK"
-    })
-})
+app.get("/api/health", (req, res) => {
+  res.json({
+    message: "API is healthy",
+    status: "ok",
+  });
+});
 
-app.listen(PORT,()=>{
-    console.log(`server is running on port${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
